d7556aff7dd44352b61aade31851771612738d6c
wouzar <wouzar@gmail.com>
2017-05-01 15:49:03 +0300

    Move Routes to trait

--- build.sbt
+++ build.sbt
@@ -6,5 +6,6 @@ scalaVersion := "2.12.2"
 
 libraryDependencies ++= Seq(
   "com.typesafe.akka" %% "akka-http" % "10.0.5",
+  "com.typesafe.akka" %% "akka-http-testkit" % "10.0.5",
   "com.typesafe.akka" %% "akka-http-xml" % "10.0.5"
 )
--- src/main/scala/com/wouzar/WebServer.scala
+++ src/main/scala/com/wouzar/WebServer.scala
@@ -6,9 +6,11 @@ import akka.http.scaladsl.marshallers.xml.ScalaXmlSupport
 import akka.http.scaladsl.model.HttpResponse
 import akka.http.scaladsl.model.StatusCodes._
 import akka.http.scaladsl.server.Directives.{entity, _}
+import akka.http.scaladsl.server.Route
 import akka.stream.ActorMaterializer
 import com.wouzar.repository.FactorsRepository
 
+import scala.concurrent.ExecutionContext
 import scala.io.StdIn
 import scala.util.Try
 import scala.xml.NodeSeq
@@ -16,63 +18,71 @@ import scala.xml.NodeSeq
 /**
   * Created by wouzar on 30.04.17.
   */
-object WebServer extends ScalaXmlSupport {
 
-  def main(args: Array[String]): Unit = {
+trait Routes extends ScalaXmlSupport {
 
-    implicit val system = ActorSystem("my-actor-system")
-    implicit val materializer = ActorMaterializer()
+  implicit val system: ActorSystem
+  implicit val materializer: ActorMaterializer
+  implicit val executionContext: ExecutionContext
 
-    implicit val executionContext = system.dispatcher
+  val calculator: Calculator
 
-    val repository = new FactorsRepository("f1.csv", "f2.csv")
-    val calculator = new Calculator(repository)
+  def parseParameter(key: String, ns: NodeSeq): Try[Int] = {
+    val newNs = ns \ key
+    Try(newNs.text.toInt)
+  }
 
-    def parseParameter(key: String, ns: NodeSeq): Try[Int] = {
-      val newNs = ns \ key
-      Try(newNs.text.toInt)
-    }
-
-    val route =
-      path("rest" / "calc") {
-        get {
-          parameter('v1.as[Int]) { v1 =>
-            complete(
-              <result>
-                {calculator.correctFactor(v1).value}
-              </result>
+  val route: Route =
+    path("rest" / "calc") {
+      get {
+        parameter('v1.as[Int]) { v1 =>
+          complete(
+            <result>
+              {calculator.correctFactor(v1).value}
+            </result>
+          )
+        }
+      } ~
+        post {
+          entity(as[NodeSeq]) { xml =>
+            val result = for {
+              v2 <- parseParameter("v2", xml)
+              v3 <- parseParameter("v3", xml)
+              v4 <- parseParameter("v4", xml)
+            } yield {
+              calculator.doCalculation(v2, v3, v4)
+            }
+            result.map { condition =>
+              complete(
+                <result>
+                  {if (condition) 0 else 1}
+                </result>)
+            }.getOrElse(
+              complete(HttpResponse(BadRequest, entity = "Wrong parameters!"))
             )
           }
-        } ~
-          post {
-            entity(as[NodeSeq]) { xml =>
-              val result = for {
-                v2 <- parseParameter("v2", xml)
-                v3 <- parseParameter("v3", xml)
-                v4 <- parseParameter("v4", xml)
-              } yield {
-                calculator.doCalculation(v2, v3, v4)
-              }
-              result.map { condition =>
-                complete(
-                  <result>
-                    {if (condition) 0 else 1}
-                  </result>)
-              }.getOrElse(
-                complete(HttpResponse(BadRequest, entity = "Wrong parameters!"))
-              )
-            }
-          }
-      }
+        }
+    }
 
-    val bindingFuture = Http().bindAndHandle(route, "localhost", 8080)
+}
 
-    println(s"Server online at http://localhost:8080/\nPress RETURN to stop...")
-    StdIn.readLine()
-    bindingFuture
-      .flatMap(_.unbind())
-      .onComplete(_ => system.terminate())
+object WebServer extends App with Routes {
 
-  }
+  override implicit val system = ActorSystem("my-actor-system")
+  override implicit val materializer = ActorMaterializer()
+  override implicit val executionContext = system.dispatcher
+
+  val repository = new FactorsRepository("f1.csv", "f2.csv")
+  val calculator = new Calculator(repository)
+
+  val (host, port) = ("localhost", 8080)
+  val bindingFuture = Http().bindAndHandle(route, host, port)
+
+  println(s"Server online at http://localhost:8080/\nPress RETURN to stop...")
+  StdIn.readLine()
+  bindingFuture
+    .flatMap(_.unbind())
+    .onComplete(_ => system.terminate())
+
 
 }
